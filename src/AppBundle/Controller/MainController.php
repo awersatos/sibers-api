372bf0b9f629cd7a1d8ce2d93982d30657a343ef
Alexandr Gordeychik <alexander.gordeychik@sibers.com>
2018-01-12 14:54:12 +0700

    Removed unnecessary files.

--- src/AppBundle/Controller/MainController.php
+++ src/AppBundle/Controller/MainController.php
@@ -8,15 +8,16 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 class MainController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/{er}")
      */
-    public function indexAction()
+    public function indexAction($er = null)
     {
         $errorHandler = $this->container->get('sibers_api.error_handler');
 
-        $errorHandler->createError(401, 999, 'Internal', 'Sibers API error');
-        return $this->render('AppBundle:Main:index.html.twig', array(
-        ));
+        if ($er == 'error') {
+            $errorHandler->createError(401, 999, 'Internal', 'Sibers API error');
+        }
+        return $this->render('AppBundle:Main:index.html.twig', array());
     }
 
 }
